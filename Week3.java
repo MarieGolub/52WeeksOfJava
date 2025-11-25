e2def61e15e9699e11c3206968c19a1c79d1985f
MarieGolub <marie.golub@icloud.com>
2025-11-25 13:18:43 +0000

    Array work in Week3
    Learnt how to binary search an array using a function

--- Week3.java
+++ Week3.java
@@ -6,12 +6,12 @@ class Week3Project {
     public static void main(String[] args) {
         char vowels[] = {'e', 'a', 'u', 'i', 'o'};
         
-        int startindex = 1;
-        int endindex = 4;
-        char key = 'a';
+        //int startindex = 1;
+        //int endindex = 4;
+        char key = 'x';
 
         //Arrays.sort(vowels, startindex, endindex);
-        int keyindex = Arrays.binarySearch(vowels, startindex, endindex, key);
+        int keyindex = Arrays.binarySearch(vowels, key);
 
         System.out.println(Arrays.toString(vowels));
         System.out.println(keyindex);
