8fb80b9633960020ffca1d4f1c2fc3f55a8d1e7a
MarieGolub <marie.golub@icloud.com>
2025-11-25 13:23:18 +0000

    Using the Arrays.fill function

--- Week3.java
+++ Week3.java
@@ -6,10 +6,10 @@ class Week3Project {
     public static void main(String[] args) {
         char vowels[] = {'e', 'a', 'u', 'i', 'o'};
         
-        //int startindex = 1;
-        //int endindex = 4;
+        int startindex = 1;
+        int endindex = 4;
 
-        Arrays.fill(vowels, 'x');
+        Arrays.fill(vowels,startindex, endindex, 'x');
 
         System.out.println(Arrays.toString(vowels));
     }
