f9bf3c0301b7f70acd832218d47a7cef0d0e5fa9
MarieGolub <marie.golub@icloud.com>
2025-12-02 13:03:10 +0000

    Using loops to print lists

--- Week4.java
+++ Week4.java
@@ -1,10 +1,9 @@
 class Week4Project{
     public static void main(String[] args){
-        for (int number = 1; number < 10; number ++){
-            System.out.printf("-------------%d times table--------------%n", number);
-            for (int multiplier = 1; multiplier < 13; multiplier++){
-                System.out.printf("%d x %d = %d", number, multiplier, number*multiplier);
-            }
+        int numbers[] = {1,2,3,4,5};
+
+        for (int number : numbers){
+            System.out.println(number);
         }
     }
 }
