32b2013876519cabca19dc6b6624c350d40e87ad
MarieGolub <marie.golub@icloud.com>
2025-12-02 10:50:13 +0000

    Recap from last week's project

--- Week4.java
+++ Week4.java
@@ -1,5 +1,13 @@
 class Week4Project{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+
+        int sum = 0;
+
+        for (int index = 0; index < 10; index++){
+            sum += numbers[index];
+        }
+
+        System.out.println(sum);
     }
 }
