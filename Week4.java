77b6d401903a9919869db77ae5cafc07f8416cf9
MarieGolub <marie.golub@icloud.com>
2025-12-03 13:28:41 +0000

    Practice with funtions of ArrayLists

--- Week4.java
+++ Week4.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 class Week4Project{
     public static void main(String[] args){
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<>();
 
+        numbers.add(5);
+        numbers.add(3);
         numbers.add(1);
+        numbers.add(2);
+        numbers.add(4);
+
+        numbers.sort(Comparator.naturalOrder());
 
         System.out.println(numbers);
     }
