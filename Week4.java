236c1f1eb800435f63d544325989c8747c11a1ef
MarieGolub <marie.golub@icloud.com>
2025-12-04 08:02:02 +0000

    Final project for Week 4

--- Week4.java
+++ Week4.java
@@ -1,24 +1,50 @@
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Scanner;
 
 class Week4Project{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
         ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<String> grades = new ArrayList<>();
 
-        numbers.add(5);
-        numbers.add(3);
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(4);
+        System.out.println("How many grades would you like to enter? ");
+        int gradenumber = scanner.nextInt();
 
-        numbers.forEach(number -> {
-            numbers.set(numbers.indexOf(number), number * 2);
-            System.out.println(number * 2);
-            });
+        int i = 0;
+        while (i < gradenumber){
+            System.out.println("Enter grade: ");
+            numbers.add(scanner.nextInt());
+            i++;
+        }
 
-        numbers.sort(Comparator.naturalOrder());
+        for (Integer number : numbers) {
+            if (number <= 100 && number >= 90){
+                grades.add("A*");
+            }
+            else if (number < 90 && number >= 80){
+                grades.add("A");
+            }
+            else if (number < 80 && number >= 70){
+                grades.add("B");
+            }
+            else if (number < 70 && number >= 60){
+                grades.add("C");
+            }
+            else if (number < 60 && number >= 50){
+                grades.add("D");
+            }
+            else if (number < 50 && number >= 40){
+                grades.add("E");
+            }
+            else {
+                grades.add("F");
+            }
+        }
 
-        System.out.println(numbers);
+        for (int j = 0; j < numbers.size(); j++){
+            System.out.println("Grade for " + numbers.get(j) + " is: " + grades.get(j));
+        }
 
     }
 }
