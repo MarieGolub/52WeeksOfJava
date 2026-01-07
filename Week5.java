672ca0190e6e4d705f26776cf7470928096e99ed
MarieGolub <marie.golub@icloud.com>
2026-01-07 12:00:14 +0000

    Started learning HashMaps

--- Week5.java
+++ Week5.java
@@ -2,6 +2,13 @@ import java.util.HashMap;
 
 class Week5Project {
     public static void main(String[] args) {
+        HashMap<String, Integer> ExamScores = new HashMap<>();
 
+        ExamScores.put("Maths", 75);
+        ExamScores.put("Chemistry", 90);
+        ExamScores.put("Physics", 85);
+        ExamScores.put("Computer Science", 95);
+
+        System.out.println(ExamScores.get("Chemistry"));
     }
 }
