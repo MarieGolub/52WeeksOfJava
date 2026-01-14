5831a0e245d0b9d51c20eb73f0957666d9d82d15
MarieGolub <marie.golub@icloud.com>
2026-01-14 09:34:19 +0000

    Creating library system with OOP

--- User.java
+++ User.java
@@ -1,7 +1,20 @@
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 
 public class User {
     public String name;
     public LocalDate birthday;
+    public ArrayList<Book> books = new ArrayList<Book>();
+
+    public void borrow(Book book){
+        this.books.add(book);
+    }
+
+    public int age() {
+        Period age = Period.between(this.birthday, LocalDate.now());
+
+        return age.getYears();
+    }
 }
--- Week5OOP.java
+++ Week5OOP.java
@@ -1,6 +1,21 @@
+
+import java.time.LocalDate;
+
 class HelloWorld {
     public static void main(String[] args) {
-        User youngerUser = new User();
+        User user = new User();
+
+        user.name = "Marie";
+        user.birthday = LocalDate.parse("2015-01-24");
+
+        Book book = new Book();
+        book.title = "The Poppy War";
+        book.author = "R.F. Kuang";
+
+        user.borrow(book);
+
+        System.out.printf("%s was born in %s, they are now %d years old", user.name, user.birthday.toString(), user.age());
+
 
     }
 }
